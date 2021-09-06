c07c0e14c4f62cbe03eea4e5966bf71e4d83ec4c
RyanJeong <wat6988@gmail.com>
2021-09-06 21:16:55 +0900

    Updated on Sep 6, 2021

--- tutorial/p01/src/string.cpp
+++ tutorial/p01/src/string.cpp
@@ -3,9 +3,9 @@
 
 class My_string{
 public:
-    My_string(char c);
-    My_string(const char* str);
     My_string(const My_string& m_str);
+    My_string(const char* str);
+    My_string(char c);
     ~My_string(void);
     int length(void) const;
     void print(void) const;
@@ -14,17 +14,25 @@ public:
     My_string& assign(const char* str);
     int capacity(void) const;
     void reserve(int size);
+    char at(int i) const;
+    My_string& insert(int pos, const My_string& m_str);
+    My_string& insert(int pos, const char* str);
+    My_string& insert(int pos, char c);
 private:
     char* content_;
     int length_;
     int capacity_;
 };
 
-My_string::My_string(char c)
+My_string::My_string(const My_string& m_str)
 {
-    length_=1;
+    length_=m_str.length_;
+    capacity_=length_;
     content_=new char[length_];
-    *content_=c;
+    //strcpy(content_,m_str.content_); not terminated with 'null' character
+    for (int i = 0; i<length_; ++i) {
+        content_[i]=m_str.content_[i];
+    }
 
     return;
 }
@@ -32,17 +40,19 @@ My_string::My_string(char c)
 My_string::My_string(const char* str)
 {
     length_=strlen(str);
+    capacity_=length_;
     content_=new char[length_];
     strcpy(content_,str);
 
     return;
 }
 
-My_string::My_string(const My_string& m_str)
+My_string::My_string(char c)
 {
-    length_=m_str.length_;
+    length_=1;
+    capacity_=length_;
     content_=new char[length_];
-    strcpy(content_,m_str.content_);
+    *content_=c;
 
     return;
 }
@@ -84,8 +94,11 @@ My_string& My_string::assign(const My_string& m_str)
         content_=new char[m_str.length_];
         capacity_=m_str.length_;
     }
-    strcpy(content_,m_str.content_);
     length_=m_str.length_;
+    //strcpy(content_,m_str.content_);
+    for (int i = 0; i<length_; ++i) {
+        content_[i]=m_str.content_[i];
+    }
 
     return *this;
 }
@@ -98,8 +111,8 @@ My_string& My_string::assign(const char* str)
         content_=new char[str_len];
         capacity_=str_len;
     }
-    strcpy(content_,str);
     length_=str_len;
+    strcpy(content_,str);
 
     return *this;
 }
@@ -110,15 +123,47 @@ int My_string::capacity(void) const
     return capacity_;
 }
 
-void My_string::reserve(int size);
+void My_string::reserve(int size)
+{
+    if (size>capacity_) {
+        char* prev = content_;
+        content_=new char[size];
+        capacity_=size;
+        for (int i = 0; i<length_; ++i) {
+            content_[i]=prev[i];
+        }
+        delete[] prev;
+    }
+
+    return;
+}
+
+char My_string::at(int i) const
+{
+    if (i<0 || i>=length_) {
+
+        return NULL;
+    }
+    else {
+
+        return content_[i];
+    }
+}
 
 int main(void)
 {
-    My_string str1("Hello, world!");
-    My_string str2(str1);
+    My_string str1{"Hello, world!"};
+    My_string str2{str1};
 
     str1.println();
     str2.println();
+
+    My_string str3{"Strings are objects that represent sequences of characters."};
+    str3.reserve(64);
+    std::cout << "Cap.: " << str3.capacity() << std::endl;
+    std::cout << "Len.: " << str3.length() << std::endl;
+    str3.println();
+
 
     return 0;
 }
