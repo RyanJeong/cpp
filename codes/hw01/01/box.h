1c582a266a549841fe5b6070d004ea6d57e48606
RyanJeong <wat6988@gmail.com>
2021-10-09 22:52:58 +0900

    Updated on Oct 9, 2021

---
+++ codes/hw01/01/box.h
@@ -0,0 +1,4 @@
+#ifndef __BOX_H__
+#define __BOX_H__
+
+#endif
---
+++ codes/hw01/01/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+class Box {
+    int width, height;
+    char fill; // box의 내부를 채우는 문자
+public:
+    Box(int w, int h) { setSize(w,h); fill='*'; }
+    void setFill(char f) { fill=f; }
+    void setSize(int w, int h) { width=w; height=h; }
+    void draw(void); // 박스 그리기
+};
+
+void Box::draw(void)
+{
+    for (int n = 0; n<height; ++n) {
+        for (int m = 0; m<width; ++m) {
+            cout << fill;
+        }
+        cout << endl;
+    }
+
+    return;
+}
+
+int main(void)
+{
+    Box b(10,2);
+    b.draw();
+
+    b.setSize(7,4);
+    b.setFill('^');
+    b.draw();
+
+    return 0;
+}
